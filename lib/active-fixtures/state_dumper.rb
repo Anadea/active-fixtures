fb69fc478b180d0401c3278f500f28981846e45f
Sergey Tokarenko <st@anahoret.com>
2016-12-05 08:49:34 +0300

    Clean dump restore.

--- lib/active-fixtures/state_dumper.rb
+++ lib/active-fixtures/state_dumper.rb
@@ -52,7 +52,7 @@ module ActiveFixtures
       end
 
       def dump_db(state_name)
-        args = ['-Fc', '-f', dump_db_file(state_name), DB_NAME]
+        args = ['-c', '-Fc', '-f', dump_db_file(state_name), DB_NAME]
         Kernel.system('pg_dump', *args)
       end
 
